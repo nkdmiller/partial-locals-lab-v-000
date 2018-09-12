c7bfe6a4908ef8e4adc5c02c8168d66dc195f490
Nate Miller <natekmiller96@gmail.com>
2018-09-12 03:10:07 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,10 +13,10 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
-  def self.search(name)
+  def self.search(search)
     matches = []
     Student.all.each do |student|
-      if student == name
+      if student.name == 
         matches << student
       end
     end
